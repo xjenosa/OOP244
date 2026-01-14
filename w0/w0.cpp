300be10e7a0753b00cd54482789e7e2627948d6a
xjenosa <alimximln@gmail.com>
2026-01-14 17:22:26 -0500

    Finished w0.cpp

---
+++ w0/w0.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout << "#### Workshop Zero ####\n";
+    cout << "Subject and Section:\n";
+    cout << "OOP244 NRA\n";
+    cout << "Name:\n";
+    cout << "Maximilian Ali\n";
+    cout << "Student Number:\n";
+    cout << "107758252\n";
+    cout << "Email:\n";
+    cout << "mali291@myseneca.ca\n";
+
+    return 0;
+}
