1054179f4b627aee41887f12113ff69340037d39
Maximilian Ali <mali291@mtrx-node05pd.dcm.senecacollege.ca>
2026-01-15 14:36:16 -0500

    Added header

--- w0/w0.cpp
+++ w0/w0.cpp
@@ -1,3 +1,20 @@
+/*************************************************************************************
+* OOP244 - 2261
+*
+* Student Name : Maximilian Ali
+* Student Email : mali291@myseneca.ca
+* Workshop #  : 0
+* Course/Section: OOP244/NRA
+*
+* I declare that this assignment is my own work in accordance with the Seneca Academic
+* Policy. No part of this assignment has been copied manually or electronically from/to
+* any other source (including web sites) or distributed to other students.
+*
+* I have done all the coding by myself and only copied the code that my professor
+* provided to complete my workshops and assignments.
+*
+**************************************************************************************/
+
 #include <iostream>
 
 using namespace std;
