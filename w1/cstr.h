60ad6fc89fa1d9e6be0125b4fc807cd88d6ca5cc
xjenosa <alimximln@gmail.com>
2026-01-20 11:03:24 -0500

    improved spacing

--- w1/cstr.h
+++ w1/cstr.h
@@ -27,19 +27,19 @@
 #include <cstddef> // for size_t
 
 namespace seneca {
-char* strcat(char* des, const char* src);
-char tolower(char ch);
-char* tolower(char* des, const char* src);
-char* tolower(char* str);
-int strcmp(const char* s1, const char* s2);
-int strcmp(const char* s1, const char* s2, size_t len);
-char* strcpy(char* des, const char* src);
-char* strcpy(char* des, const char* src, size_t len);
-char* strncpy(char* des, const char* src, size_t len);
-size_t strlen(const char* str);
-const char* strstr(const char* str, const char* toFind);
-bool isalpha(char ch);
-bool isspace(char ch);
+    char* strcat(char* des, const char* src);
+    char tolower(char ch);
+    char* tolower(char* des, const char* src);
+    char* tolower(char* str);
+    int strcmp(const char* s1, const char* s2);
+    int strcmp(const char* s1, const char* s2, size_t len);
+    char* strcpy(char* des, const char* src);
+    char* strcpy(char* des, const char* src, size_t len);
+    char* strncpy(char* des, const char* src, size_t len);
+    size_t strlen(const char* str);
+    const char* strstr(const char* str, const char* toFind);
+    bool isalpha(char ch);
+    bool isspace(char ch);
 }
 
 #endif // !SENECA_CSTR_H
