4c9a60a0405afff13646bbe5f83cfc7d99fd80f3
xjenosa <alimximln@gmail.com>
2026-02-07 14:54:26 -0500

    added header

--- w4/Account.h
+++ w4/Account.h
@@ -14,6 +14,27 @@
 //
 /////////////////////////////////////////////////////////////////
 ***********************************************************************/
+
+/*************************************************************************************
+* OOP244 - 2261
+*
+* Student Name : Maximilian Ali
+* Student Email : mali291@myseneca.ca
+* Workshop #  : 3
+* Course/Section: OOP244/NRA
+*
+* I declare that this assignment is my own work in accordance with the Seneca Academic
+* Policy. No part of this assignment has been copied manually or electronically from/to
+* any other source (including web sites) or distributed to other students.
+*
+* I have done all the coding by myself and only copied the code that my professor
+* provided to complete my workshops and assignments.
+*
+* I used AI to help me with the following:
+*  - Debugging and format optimization, as to why my outputs don't match the expected results.
+*
+**************************************************************************************/
+
 #ifndef SENECA_ACCOUNT_H_
 #define SENECA_ACCOUNT_H_
 #include <iostream>
